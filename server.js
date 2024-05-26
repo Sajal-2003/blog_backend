4daedbdafbfa2911bb3b33035a1e84434d6ecd83
Sajal Aggarwal <sajal72003@gmail.com>
2024-05-26 15:10:32 +0530

    build commit

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({ credentials: true, origin: "https://blogifyblogs.netlify.app/" })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.json());
