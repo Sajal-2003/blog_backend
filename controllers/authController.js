bd1c9adce8db3c70bd0bd95ef860ccc2836be1ec
Sajal Aggarwal <sajal72003@gmail.com>
2024-05-28 13:21:22 +0530

    last backend commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -57,7 +57,7 @@ const loginController = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
     console.log(token);
